21a7eb07d388fa1fb074cbfdc348db28dd65447d
DOREEN MENSAH <doreenradiance@gmail.com>
2023-06-22 10:27:10 +0000

    wip

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ import BlogInfo from './pages/BlogInfo';
 import CheckoutSideForm1 from './pages/CheckoutForm1';
 import HomePage from './pages/HomePage';
 import ShoesCarousel from './pages/ShoesCarousel';
-import ProfileOrderListInfo from './pages/ProfileOrderListInfo'
 import 'primeicons/primeicons.css';
 
 function App() {
