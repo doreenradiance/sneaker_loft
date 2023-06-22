12a52bed5a8f8a4a515aac6cb75464985068e2a5
DOREEN MENSAH <doreenradiance@gmail.com>
2023-06-22 10:21:31 +0000

    wip

--- src/components/CheckoutSideForm.js
+++ src/components/CheckoutSideForm.js
@@ -4,32 +4,49 @@ import React from "react";
 function CheckoutSideForm() {
     return (
         <>
-            <div style={{ padding: "2rem" }} >
-                <h2>Checkout</h2>
-                <div>
+            <div style={{ padding: "1rem" }} >
+                <h2 className="checkout">Checkout</h2>
                     <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-                        <p>Contact Information</p>
-                        <div style={{ display: "flex", flexDirection: "row", }}> <p style={{ fontSize: "12px" }}>Already have an account?</p>
-                            <p style={{ textDecoration: "underline", fontSize: "12px" }}>Login</p>
+                        <p className="contact_info">Contact Information</p>
+                        <div style={{ display: "flex", flexDirection: "row", }}>
+                            <p style={{ fontSize: "12px", marginRight: "0.5rem" }}>Already have an account?</p>
+                            <p className="login_text">Login</p>
                         </div>
                     </div>
 
-                    <input style={{ width: "99%", height: "2rem" }} placeholder="Email Address" />
+                    <input className="checkout_inputs" style={{ width: "99%", height: "2rem", marginTop: "0.5rem" }} placeholder="Email Address" />
                     <div style={{ display: "flex", flexDirection: "row", }}>
-                        <input type="checkbox" id="" name="" value="" style={{ marginRight: "0.5rem" }} />
+                        <input className="checkout_inputs" type="checkbox" id="" name="" value="" style={{ marginRight: "0.5rem" }} />
                         <p>Send me news and offers by email</p>
                     </div>
-                    <p style={{}}>Shipping Address</p>
-                    <select style={{ width: "99.8%", height: "2.5rem" }} name="Country/Region" id="Country/Region">
+                    <p style={{ marginTop: "2rem" }}>Shipping Address</p>
+                    <select className="checkout_inputs" style={{ width: "99.8%", height: "2.5rem" }} name="Country/Region" id="Country/Region">
                         <option value="Country/Region">Country/Region</option>
                     </select>
-                    <div style={{ display: "flex", flexDirection: "row", marginTop: "1rem", justifyContent: "space-between" }}>
-                        <input style={{ width: "47%", height: "2rem", }} placeholder="First Name" />
-                        <input style={{ width: "47%", height: "2rem", }} placeholder="Last Name" />
+                    <div
+                        style={{ display: "flex", flexDirection: "row", marginTop: "2rem", }}
+                    >
+                        <input className="checkout_inputs" style={{ width: "24rem", height: "2rem" }} placeholder="First Name" />
+                        <input className="checkout_inputs" style={{ width: "24rem", height: "2rem" }} placeholder="Last Name" />
                     </div>
-                    <input placeholder="Company (Optional)" style={{ width: "99%", height: "2rem", marginTop: "1rem" }} />
+                    <input className="checkout_inputs" placeholder="Street and house number" style={{ width: "99%", height: "2rem", marginTop: "2rem" }} />
+                    <input className="checkout_inputs" placeholder="Apartment,premises,etc (Optional)" style={{ width: "99%", height: "2rem", marginTop: "2rem" }} />
+                    <div
+                        style={{ display: "flex", flexDirection: "row", marginTop: "1rem", justifyContent: "space-between" }}
+                    >
+                        <input className="checkout_inputs" style={{ width: "160px", height: "2rem" }} placeholder="Post code" />
+                        <input className="checkout_inputs" style={{ width: "160px", height: "2rem", }} placeholder="Town" />
+                        <select className="checkout_inputs" style={{ width: "200px", height: "2.3rem", }}>
+                            <option>Province</option>
+                        </select>
+                    </div>
+                    <input placeholder="Telephone" className="checkout_inputs" style={{ width: "99%", height: "2rem", marginTop: "2rem" }} />
+                    <div style={{ display: "flex", flexDirection: "row" }}>
+                        <input type="checkbox" className="checkout_inputs" id="" name="" value="" style={{ marginRight: "0.5rem" }} />
+                        <p> Save this information for later use.</p>
+                    </div>
+                    <button style={{ height: "3rem", width: "30rem", color: "#F3DF77", backgroundColor: "#000000", borderRadius: "0.3rem", marginTop: "2.5rem" }}>Continue Shipping</button>
                 </div>
-            </div>
         </>
     )
 }
---
+++ src/pages/CheckoutForm2.js
@@ -0,0 +1,37 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import CheckoutSideForm from "../components/CheckoutSideForm";
+import CartSummary from "../components/CartSummary";
+import "../styles/cart.scss"
+import "../styles/checkOut.scss";
+import CheckoutSideImages from "../components/CheckoutSideImages";
+
+function CheckoutForm1() {
+    return (
+        <>
+            <NavBar />
+            <div className="grid checkout_main_div" >
+                <div className="col-12 md:col-6 lg:col-6">
+                    <CheckoutSideForm />
+                </div>
+
+                <div style={{ marginTop: "9rem" }} className="col-12 md:col-6 lg:col-6">
+                    <CheckoutSideImages />
+                    <div >
+                        <CartSummary />
+                    </div>
+                    <div style={{ backgroundColor: 'white', width: "29rem", padding: "1rem", marginTop: "2rem" }}>
+                        <p style={{ fontWeight: "700", marginBottom: "0.5rem", fontSize: "18px" }}>Shipping Address</p>
+                        <p className="shipping_details" >James Anderson</p>
+                        <p className="shipping_details">17 Allis Miles</p>
+                        <p className="shipping_details" >Idaho 8238</p>
+                        <p className="shipping_details">+1 434 723 3489</p>
+                    </div>
+                </div >
+            </div>
+            <Footer />
+        </>
+    )
+}
+export default CheckoutForm1;
